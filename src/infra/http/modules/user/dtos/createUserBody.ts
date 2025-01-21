9cdc1da3f6d39700b56c1d69a997267496ec39a1
Daniel da Silva Ramos Bueno <jj.danielbr10@gmail.com>
2025-01-21 14:24:15 -0300

    feat: add user view model

--- src/infra/http/modules/user/dtos/createUserBody.ts
+++ src/infra/http/modules/user/dtos/createUserBody.ts
@@ -1,6 +1,18 @@
+import { IsEmail, IsNotEmpty, IsString } from "class-validator"
+
 export class CreateUserBody {
+  @IsString()
+  @IsNotEmpty()
   name: string
+
+  @IsEmail()
+  @IsString()
+  @IsNotEmpty()
   email: string
+
+  @IsString()
+  @IsNotEmpty()
   password: string
+
   createdAt: Date
 }
