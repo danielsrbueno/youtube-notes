b7cc7a0fa53f3ac295e129e538536cf20af5e8e9
Daniel da Silva Ramos Bueno <jj.danielbr10@gmail.com>
2025-01-22 20:57:27 -0300

    feat: add jwt strategy

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,17 @@ import { Module } from '@nestjs/common'
 import { UserModule } from './infra/http/modules/user/user.module'
 import { DatabaseModule } from './infra/database/database.module'
 import { AuthModule } from './infra/http/modules/auth/auth.module'
+import { APP_GUARD } from '@nestjs/core'
+import { jwtAuthGuard } from './infra/http/modules/auth/guards/jwtAuth.guard'
 
 @Module({
-  imports: [UserModule, DatabaseModule, AuthModule],
+  imports: [UserModule, DatabaseModule, AuthModule, ],
   controllers: [],
-  providers: [],
+  providers: [
+    {
+      provide: APP_GUARD,
+      useClass: jwtAuthGuard,
+    },
+  ],
 })
 export class AppModule {}
--- src/infra/http/modules/auth/auth.controller.ts
+++ src/infra/http/modules/auth/auth.controller.ts
@@ -1,12 +1,27 @@
-import { Controller, HttpCode, HttpStatus, Post, Request, UseGuards } from "@nestjs/common"
-import { AuthGuard } from "@nestjs/passport"
+import { Controller, Get, HttpCode, HttpStatus, Post, Request, UseGuards } from "@nestjs/common"
+import { AuthRequestModel } from "./models/authRequestModel"
+import { SignInUseCase } from "src/modules/auth/useCases/signInUseCase/signInUseCase"
+import { LocalAuthGuard } from "./guards/localAuth.guard"
+import { jwtAuthGuard } from "./guards/jwtAuth.guard"
+import { Public } from "./decorators/isPublic"
+import { AuthenticatedRequestModel } from "./models/authenticatedRequestModel"
 
 @Controller()
 export class AuthController{
+  constructor(private signInUseCase: SignInUseCase) {}
+
   @Post('singIn')
+  @Public()
   @HttpCode(HttpStatus.OK)
-  @UseGuards(AuthGuard('local'))
-  async signIn(@Request() request: any) {
+  @UseGuards(LocalAuthGuard)
+  async signIn(@Request() request: AuthRequestModel) {
+    const access_token = await this.signInUseCase.execute({ user: request.user })
+    return { access_token }
+  }
+
+  @Get('test')
+  @UseGuards(jwtAuthGuard)
+  async test(@Request() request: AuthenticatedRequestModel) {
     return request.user
   }
 }
